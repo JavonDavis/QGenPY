105a25f807771bf179e3b4c8da12196c7b1871e5
Howard <howarde.jr@hotmail.com>
2016-04-06 10:27:35 -0500

    different moodle xmls being created for each yml question file

--- tests/tests.py
+++ tests/tests.py
@@ -17,28 +17,19 @@ TEST_SETS_YML_PATH = os.getcwd() + "/tests/test_sets.yml"
 class TestSuite(unittest.TestCase):
     """ test cases."""
 
-    # only one setup call
-    mxb.setup()
-
-    # have to keep tests in one block
-    def tests(self):
-        # test_a_hello_world
+    def test_a_hello_world(self):
         qgen.test()
 
-        # test_b_imports
+    def test_b_imports(self):
         built_in_size = len(built_in)
         qgen.build_moodle_xml(TEST_IMPORTS_YML_PATH, question="SimplePolynomial", number_of_questions=2)
         self.assertGreater(len(qgen.functions), built_in_size, "No functions were imported")
 
-        # test_b_generate_moodle_xml
+    def test_b_generate_moodle_xml(self):
         qgen.build_moodle_xml(TEST_SIMPLE_YML_PATH, question="SimpleAddition", number_of_questions=2)
 
-        # test_a_sets
+    def test_a_sets(self):
         qgen.build_moodle_xml(TEST_SETS_YML_PATH, question="SimpleSets", number_of_questions=2)
-
-        # only one end tag call
-        mxb.build_quiz_end_tag()
-
 
 if __name__ == '__main__':
     unittest.main()
